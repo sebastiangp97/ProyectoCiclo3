16c9bf08cc41c9291a045d833d385cc01dbe6145
sebastiangp97 <juansgarcia97@gmail.com>
2021-10-18 20:50:52 -0500

    Actualizacion category

--- src/main/java/co/usa/ciclo3/Ciclo3/model/Category.java
+++ src/main/java/co/usa/ciclo3/Ciclo3/model/Category.java
@@ -18,14 +18,45 @@ import java.util.List;
 @Table(name="category")
 public class Category implements Serializable{
 
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Integer id;
-	    private String name;
-	    private String description;
-	    
-	    
-	    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy= "category")
-	    @JsonIgnoreProperties("category")
-	    private List<Bicicletas> bikes;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String description;
+
+	@OneToMany(cascade = {CascadeType.PERSIST},mappedBy= "category")
+	@JsonIgnoreProperties("category")
+	private List<Bicicletas> bikes;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Bicicletas> getBikes() {
+		return bikes;
+	}
+
+	public void setBikes(List<Bicicletas> bikes) {
+		this.bikes = bikes;
+	}
 }
